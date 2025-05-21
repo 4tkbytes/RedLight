69389ecb1fd1933b29c100eda89332b86dc9339a
tk <4tkbytes@pm.me>
2025-05-21 14:50:23 +1000

    Resource management works hooray!

---
+++ RedLight/Utils/RLFiles.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace RedLight.Utils;
+
+public static class Files
+{
+    public static string EmbeddedResourceAsString(string resourceName)
+    {
+        var assembly = Assembly.GetCallingAssembly(); // Or Assembly.GetExecutingAssembly() depending on your needs
+
+        using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+        {
+            if (stream == null)
+            {
+                throw new FileNotFoundException($"Embedded resource '{resourceName}' not found.");
+            }
+
+            using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
