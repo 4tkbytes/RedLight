d957bdbc7ef980b1a4378611daf06058bbbef5f4
tk <4tkbytes@pm.me>
2025-06-14 23:30:38 +1000

    completely forgot about an error

--- ExampleGame/Program.cs
+++ ExampleGame/Program.cs
@@ -12,14 +12,12 @@ class Program
     {
         // Initialise scenes
         var scene1 = new TestingScene1();
-        var lightingScene = new LightingScene();
 
         // Create engine instance
-        var engine = new RLEngine(1280, 720, "RedLight Game Engine Editor", lightingScene, args);
+        var engine = new RLEngine(1280, 720, "RedLight Game Engine Editor", scene1, args);
 
         // add scenes to scene manager
         SceneManager.Instance.Add("testing_scene_1", scene1);
-        SceneManager.Instance.Add("lighting_scene", lightingScene);
 
         // run
         engine.Run();
